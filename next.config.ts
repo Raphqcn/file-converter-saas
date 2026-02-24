7eb9fc4ad6e2772239608487fe5676cb7067ab39
Asciien <quinchon.raphael.061@gmail.com>
2026-02-24 14:06:38 +0100

    CSP update

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
         headers: [
           { key : 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
           { key : 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key : 'Content-Security-Policy',
+            value: "default-src 'self'; " +
+                   "script-src 'self' 'wasm-unsafe-eval' https://unpkg.com; " +
+                   "worker-src 'self' blob:; " +
+                   "connect-src 'self' https://unpkg.com; " +
+                   "object-src 'none; " +
+                   "baseè-url 'self';"
+          }
         ],
       },
     ]
